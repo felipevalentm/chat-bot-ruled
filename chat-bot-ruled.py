7e4e7572a4c19f054e455b6d6c9798075d751251
felipevalntim <luizvalentim2002@gmail.com>
2026-03-22 14:47:21 -0300

    melhoras e adicao de funcao para identificar intencao

--- chat-bot-ruled.py
+++ chat-bot-ruled.py
@@ -5,30 +5,70 @@ informacoes = {
     "vendas" : ["comercial@gmail.com", "24993956589"],
     }
 
-interacao = {
-    "saudacao": ["oi", "olá", "bom", "dia", "boa", "tarde", "tudo", "bem"],
-    "despedida": ["tchau", "fim", "encerrar"]
+interacoes = {
+    "saudacao": [
+    "oi", "olá", "ola", "oie", "oii",
+    "eai", "eaí", "eaí", "fala", "falae",
+    "salve", "opa", "opa tudo bem",
+    "bom dia", "boa tarde", "boa noite",
+    "tudo bem", "td bem", "blz", "beleza",
+    "como vai", "como vc tá", "como você tá",
+    "tranquilo", "suave",
+    "hello", "hi", "hey"],
+
+    "despedida": [
+    "tchau", "tchau tchau", "até mais", "até logo", "até",
+    "falou", "fui", "flw", "vlw", "valeu",
+    "encerrar", "finalizar", "terminar", "fim",
+    "já resolveu", "resolvido", "era isso", "só isso",
+    "obrigado era isso", "valeu era isso",
+    "até breve", "nos vemos"],
+
+    "ajuda": [
+    "ajuda", "socorro", "help", "preciso de ajuda",
+    "pode ajudar", "me ajuda", "me ajude",
+    "como funciona", "como faz", "como fazer",
+    "não entendi", "nao entendi", "não sei", "nao sei",
+    "dúvida", "duvida", "tenho uma dúvida", "tenho duvida",
+    "explica", "explicar", "pode explicar",
+    "quero saber", "me informa", "informação", "informacoes",
+    "o que fazer", "como resolver", "resolver isso",
+    "não consigo", "nao consigo", "deu erro", "erro",
+    "problema", "tá dando erro", "ta dando erro"]
+
     }
 
 contexto = None
-frase_inicial = input("Usuário: ").replace(".", "").replace("?", "").replace("!", "").replace(",", "").lower().split()
+mensagem = input("Usuário: ").replace(".", "").replace("?", "").replace("!", "").replace(",", "").lower()
 
-while True:
-    for x in frase_inicial:
-        if x in interacao["saudacao"]:
-            contexto = 1
-            print("Bot: ", "bom dia! qual seu nome?")            
-            if contexto == 1:
-                resposta_nome = input("Usuário: ").replace(".", "").replace("?", "").replace("!", "").replace(",", "").lower()
-                contexto = 2
-                if contexto == 2:
-                    print("Bot: " , f"{resposta_nome}, qual área deseja contatar? ")
-                    resposta_area = input("Usuário: ").replace(".", "").replace("?", "").replace("!", "").replace(",", "").lower()
-                    if resposta_area in informacoes:
-                        print(", ".join(informacoes[resposta_area]))
-                    else:
-                        print("área inválida!")
-        elif x in interacao["despedida"]:    
-            contexto = 0
-            print("conversa encerrada...")
-    break
+def definindo_intencao():
+    for tipo, palavras in interacoes.items():
+        for termo in palavras:
+            if termo in mensagem:
+                return tipo
+    return "desconhecido"
+
+intencao_definida = definindo_intencao()
+contexto = intencao_definida
+
+if contexto == "saudacao" or contexto == "ajuda":
+        nome = input("bot: qual seu nome? \n"
+             "usuário: ")
+        print(f"{nome}, qual area deseja contatar? ")
+        area = input("usuário: ").replace(".", "").replace("?", "").replace("!", "").replace(",", "").lower()
+        if area == "rh":
+            print(f"email: {informacoes['rh'][0]}")
+            print(f"contato: {informacoes['rh'][1]}")
+        elif area == "financeiro":
+            print(f"email: {informacoes['financeiro'][0]}")
+            print(f"contato: {informacoes['financeiro'][1]}")
+        elif area == "comercial":
+            print(f"email: {informacoes['vendas'][0]}")
+            print(f"contato: {informacoes['vendas'][1]}")
+        else:
+            print("não entendemos.. encerrando chat")
+elif contexto == "despedida":
+        print("encerrando.. tchau")
+     
+
+    
